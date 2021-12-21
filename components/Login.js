b0e8d34949b07a88ff8f9b1076bd7449811e096f
Rakibul71 <rakibulislam202019@gmail.com>
2021-12-21 13:01:02 +0600

    first commit

---
+++ components/Login.js
@@ -0,0 +1,11 @@
+// short cut to create this template _rfce
+
+function Login() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">This is login page</h1>
+    </div>
+  );
+}
+
+export default Login;
